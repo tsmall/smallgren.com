4eecc20333feef1c799c74ee0a59a1bdc88aad56
Tom Small <tsmall3@gmail.com>
2016-05-07 18:32:28 -0400

    refactor: use ring-defaults not compojure.handler
    When I read the compojure.handler docs I saw that the compojure.handler
    namespace was deprecated. The author recommends using ring-defaults
    instead. This is a new site. No reason to use deprecated code.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-defaults "0.2.0"]
                  [ring/ring-jetty-adapter "1.4.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler wedding-site.handler/app}
--- src/wedding_site/handler.clj
+++ src/wedding_site/handler.clj
@@ -1,15 +1,15 @@
 (ns wedding-site.handler
   (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :refer :all]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (wrap-defaults app-routes site-defaults))
 
 (defn run
   [options]
