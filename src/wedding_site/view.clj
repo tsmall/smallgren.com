9f9e167e69d3f0195c9f5de499dfc13b81fb0708
Tom Small <tsmall3@gmail.com>
2016-05-31 00:03:12 -0400

    fix: add viewport meta tag
    Add the "viewport" meta tag to tell user agents that the site is
    responsive and they should show it at their native size.

--- src/wedding_site/view.clj
+++ src/wedding_site/view.clj
@@ -9,6 +9,8 @@
   (h/html5
    [:head
     [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport"
+            :content "width=device-width, initial-scale=1.0"}]
     [:title title]
     (h/include-css "/r/styles/main.css")]
    [:body
