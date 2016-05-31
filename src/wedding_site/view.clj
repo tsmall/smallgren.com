b4f763c8cc14156d0ed0b35e6784fcbad7394ce9
Tom Small <tsmall3@gmail.com>
2016-05-30 23:44:06 -0400

    refactor: extract page template
    There are going to be multiple pages that should all share the same
    layout, stylesheet, etc. So extract the shared code from the road-trip
    view function into a new sharable page function. Also rename wedding.css
    to main.css, since it's likely to apply to more than the wedding site.

--- src/wedding_site/view.clj
+++ src/wedding_site/view.clj
@@ -3,29 +3,34 @@
             [wedding-site.db :as db]
             [wedding-site.utils :as utils]))
 
-(defn reception-list
-  "Show all of the receptions."
+(defn- page
+  "Template for a full HTML page."
+  [title & body]
+  (h/html5
+   [:head
+    [:meta {:charset "utf-8"}]
+    [:title title]
+    (h/include-css "/r/styles/main.css")]
+   [:body
+    [:header
+     [:h1.page-title title]]
+    body]))
   []
+(defn road-trip []
   (let [receptions (db/all-receptions)
         receptions-by-date (sort-by :day receptions)]
-    (h/html5
-     [:head
-      [:meta {:charset "utf-8"}]
-      [:title "Reception tour"]
-      (h/include-css "/r/styles/wedding.css")]
-     [:body
-      [:header
-       [:h1.page-title "Reception tour"]]
-      [:section#intro
-       [:p
-        "We're going on a road trip right after our wedding, "
-        "so we can celebrate with our family and friends. "]
-       [:p
-        "We'll keep this page updated with the latest information. "
-        "We hope you can join us!"]]
-      [:section#reception-list
-       [:ol.item-list
-        (for [r receptions-by-date]
-          [:li.item-list__item
-           [:h1.item-list__heading (:city r) ", " (:state r)]
-           [:h2.item-list__subhead (utils/formatted-date (:day r))]])]]])))
+    (page
+     "Road trip"
+     [:section#intro
+      [:p
+       "We're going on a road trip right after our wedding, "
+       "so we can celebrate with our family and friends. "]
+      [:p
+       "We'll keep this page updated with the latest information. "
+       "We hope you can join us!"]]
+     [:section#reception-list
+      [:ol.item-list
+       (for [r receptions-by-date]
+         [:li.item-list__item
+          [:h1.item-list__heading (:city r) ", " (:state r)]
+          [:h2.item-list__subhead (utils/formatted-date (:day r))]])]])))
