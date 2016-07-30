6d4e0fe58b0d3c8870c1bbe833f646e92ff59b69
Tom Small <tsmall3@gmail.com>
2016-07-30 14:24:53 -0400

    refactor: group admin routes

--- src/wedding_site/routes/wedding_admin.clj
+++ src/wedding_site/routes/wedding_admin.clj
@@ -2,9 +2,11 @@
   (:require [clojurewerkz.route-one.core :refer [defroute]]))
 
 (defroute home "/wedding/a")
+(defroute login "/wedding/a/login")
+
+;;;; Receptions
 (defroute reception-list "/wedding/a/receptions")
 (defroute new-reception "/wedding/a/receptions/new")
 (defroute single-reception "/wedding/a/receptions/:day")
 (defroute edit-reception "/wedding/a/receptions/:previous-day")
 (defroute delete-reception "/wedding/a/receptions/:day/delete")
-(defroute login "/wedding/a/login")
